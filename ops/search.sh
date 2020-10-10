08a2f1bbdcee32d87a8b0b9c92cc6a778eb461bf
bohendo <bohende@gmail.com>
2020-10-10 13:24:40 +0530

    ✨ search/replace helpers

---
+++ ops/search.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+target="$1";
+shift;
+
+if [[ -z "$target" ]]
+then echo "One arg required: bash ops/search.sh <target>" && exit 1
+fi
+
+grep "$@" --exclude=*.swp --exclude=*.pdf --color=auto -r "$target" \
+  Makefile \
+  ops \
+  package.json \
+  src.sol \
+  src.ts
